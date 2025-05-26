4b4145409db8af6234ccaef8eac3238c6c908009
Joao Vitor <Joao Vitor@revenda.local>
2025-05-26 16:43:38 -0300

    Correcao-Erros

--- app.py
+++ app.py
@@ -176,4 +176,7 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
+    
