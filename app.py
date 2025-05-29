6353e9a79e4ab7b06e20294bc1821879019e86ab
Taruma-JoaoV <joaov.taruma@gmail.com>
2025-05-29 13:36:30 -0300

    Adapt para Docker

--- app.py
+++ app.py
@@ -176,7 +176,8 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
 
     
